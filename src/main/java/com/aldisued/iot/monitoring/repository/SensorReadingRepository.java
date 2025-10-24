1a5e37fbffd9ea1c1889c8958c49b09515622ab9
zsoltgolya <1.golya.zsolt@gmail.com>
2025-10-24 22:18:45 +0200

    feat: implement the getAverageTemperature in MeasurementService

--- src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
+++ src/main/java/com/aldisued/iot/monitoring/repository/SensorReadingRepository.java
@@ -3,5 +3,10 @@ package com.aldisued.iot.monitoring.repository;
 import com.aldisued.iot.monitoring.entity.SensorReading;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 public interface SensorReadingRepository extends JpaRepository<SensorReading, String> {
+
+  List<SensorReading> findByTimestampBetween(LocalDateTime from, LocalDateTime to);
 }
--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementService.java
@@ -1,11 +1,13 @@
 package com.aldisued.iot.monitoring.service;
 
+import com.aldisued.iot.monitoring.entity.SensorReading;
 import com.aldisued.iot.monitoring.entity.SensorType;
 import com.aldisued.iot.monitoring.repository.SensorReadingRepository;
+import org.springframework.stereotype.Service;
+
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
-import org.springframework.stereotype.Service;
 
 @Service
 public class MeasurementService {
@@ -23,8 +25,16 @@ public class MeasurementService {
   }
 
   public Optional<Double> getAverageTemperature(LocalDateTime from, LocalDateTime to) {
-    // TODO: Task 7
-    return Optional.empty();
-  }
+    if(from.isAfter(to)){
+      throw new IllegalArgumentException("'from' must be before 'to'");
+    }
 
+    return sensorReadingRepository.findByTimestampBetween(from, to).stream()
+      .filter(sr-> SensorType.TEMPERATURE.equals(sr.getSensor().getType()))
+      .mapToDouble(SensorReading::getValue)
+      .average()
+      .stream()
+      .boxed()
+      .findFirst();
+  }
 }
