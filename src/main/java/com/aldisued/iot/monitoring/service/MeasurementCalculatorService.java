787392c15b920ff20903001d35b95033e62fe333
zsoltgolya <1.golya.zsolt@gmail.com>
2025-10-25 01:19:50 +0200

    feat: implement the getMovingAverage in MeasurementCalculatorService

--- src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
+++ src/main/java/com/aldisued/iot/monitoring/service/MeasurementCalculatorService.java
@@ -4,14 +4,13 @@ package com.aldisued.iot.monitoring.service;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class MeasurementCalculatorService {
 
   public List<Double> filterByAverageDeviation(List<Double> values, Double deviation) {
-    if (deviation < 0.0 || deviation > 1.0) {
-      throw new IllegalArgumentException("Deviation must be between 0.0 and 1.0");
-    }
+    validDeviation(deviation);
 
     double average = values.stream()
       .mapToDouble(v -> v)
@@ -28,8 +27,30 @@ public class MeasurementCalculatorService {
   }
 
   public List<Double> getMovingAverage(List<Double> data, int windowSize) {
-    // TODO: Task 10
-    return List.of();
+    validWindowsSize(data.size(), windowSize);
+
+    return IntStream.rangeClosed(0, data.size() - windowSize)
+      .mapToObj(i -> data.subList(i, i + windowSize).stream()
+        .mapToDouble(Double::doubleValue)
+        .average()
+        .orElse(0.0))
+      .toList();
+  }
+
+  private void validDeviation(Double deviation) {
+    if (deviation < 0.0 || deviation > 1.0) {
+      throw new IllegalArgumentException("Deviation must be between 0.0 and 1.0");
+    }
+  }
+
+  private void validWindowsSize(int sizeOfData, int windowSize) {
+    if (windowSize <= 0) {
+      throw new IllegalArgumentException("windowSize must be greater than 0.");
+    }
+
+    if (windowSize > sizeOfData) {
+      throw new IllegalArgumentException("windowSize cannot be greater than the number of data.");
+    }
   }
 
 }
