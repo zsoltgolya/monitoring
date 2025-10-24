365f8f2f0d2f0d13654868eb7bc2f2f43724cd95
zsoltgolya <1.golya.zsolt@gmail.com>
2025-10-24 00:47:00 +0000

    feat: add new endpoint in AlertController, the latest alert by sensorId

--- src/main/java/com/aldisued/iot/monitoring/repository/AlertRepository.java
+++ src/main/java/com/aldisued/iot/monitoring/repository/AlertRepository.java
@@ -3,5 +3,10 @@ package com.aldisued.iot.monitoring.repository;
 import com.aldisued.iot.monitoring.entity.Alert;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface AlertRepository extends JpaRepository<Alert, String> {
+
+  Optional<Alert> findFirstBySensorIdOrderByTimestampDesc(UUID sensorId);
 }
