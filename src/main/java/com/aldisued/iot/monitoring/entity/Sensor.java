e368eb1dd1ec24e94c04df16e34c54e6e877aba8
zsoltgolya <1.golya.zsolt@gmail.com>
2025-10-24 00:06:14 +0000

    feat: add exception handling when the sensor name not unique and without name

--- src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
+++ src/main/java/com/aldisued/iot/monitoring/entity/Sensor.java
@@ -10,12 +10,13 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 
-@Table(name = "sensors")
+@Table(name = "sensors", uniqueConstraints = @UniqueConstraint(columnNames = "name"))
 @Entity
 public class Sensor {
 
